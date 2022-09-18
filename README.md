9d4c0b9eefc480a1dbec3f6842a5b8a28fbd5955
BETTERFUTURE4 <rerub0831@gmail.com>
2022-09-18 21:23:02 +0900

    feat: 2단계 Junit4TestRunner 구현

--- README.md
+++ README.md
@@ -32,6 +32,6 @@
 #### reflection
 
 - [x] Junit3TestRunner 클래스의 모든 테스트를 통과시킨다.
-- [ ] Junit4TestRunner 클래스의 모든 테스트를 통과시킨다.
+- [x] Junit4TestRunner 클래스의 모든 테스트를 통과시킨다.
 - [ ] ReflectionTest 클래스의 모든 테스트를 통과시킨다.
 - [ ] ReflectionsTest 클래스의 모든 테스트를 통과시킨다.
--- reflection/src/test/java/reflection/Junit4TestRunner.java
+++ reflection/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,40 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
 
     @Test
-    void run() throws Exception {
+    void run() {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Junit4Test junit4Test = new Junit4Test();
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        // TODO Junit4Test 에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] methods = clazz.getMethods();
+        List<Method> methodsList = Arrays.stream(methods)
+                .filter(this::isMatch)
+                .collect(Collectors.toList());
+
+        for (Method method : methodsList) {
+            invoke(junit4Test, method);
+        }
+    }
+
+    private boolean isMatch(Method method) {
+        return Arrays.stream(method.getDeclaredAnnotations())
+                .anyMatch(annotation -> annotation.getClass().isInstance(MyTest.class));
+    }
+
+    private void invoke(Object obj, Method method) {
+        try {
+            method.invoke(obj);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 }
