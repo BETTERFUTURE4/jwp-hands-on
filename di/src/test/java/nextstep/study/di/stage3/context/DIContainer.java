a4d1d2e1361588c81a933b11ebffed71ebc765c6
BETTERFUTURE4 <rerub0831@gmail.com>
2022-09-30 14:49:51 +0900

    feat: DI 3단계 모범답안에 맞춰 재구현

--- di/src/test/java/nextstep/study/di/stage3/context/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage3/context/DIContainer.java
@@ -1,74 +1,52 @@
 package nextstep.study.di.stage3.context;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.lang.reflect.Field;
 import java.util.Set;
+import java.util.stream.Collectors;
+import nextstep.study.ConsumerWrapper;
+import nextstep.study.FunctionWrapper;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
  */
 class DIContainer {
+
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        beans = new HashSet<>();
-        init(classes);
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
     }
 
-    private void init(Set<Class<?>> classes) {
-        for (Class<?> aClass : classes) {
-            beans.add(convertToObject(aClass));
+    private Set<Object> createBeans(Set<Class<?>> classes) {
+        return classes.stream()
+                .map(FunctionWrapper.apply(Class::getDeclaredConstructor))
+                .peek(constructor -> constructor.setAccessible(true))
+                .map(FunctionWrapper.apply(Constructor::newInstance))
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private void setFields(final Object bean) {
+        for (final var field : bean.getClass().getDeclaredFields()) {
+            setField(bean, field);
         }
     }
 
-    private Object convertToObject(Class<?> aClass) {
-        try {
-            Constructor<?> constructor = getConstructor(aClass);
-            List<?> parameterBeans = getParameterBeans(constructor.getParameterTypes());
-            return constructor.newInstance(parameterBeans.toArray());
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException("No Constructor!");
-        }
-    }
-
-    private Constructor<?> getConstructor(Class<?> aClass) {
-        return aClass.getDeclaredConstructors()[0];
-    }
-
-    private List<?> getParameterBeans(Class<?>... parameterTypes) {
-        List<Object> parameters = new ArrayList<>();
-        for (Class<?> parameterType : parameterTypes) {
-            Object bean = getBean(parameterType);
-            parameters.add(bean);
-        }
-        return parameters;
+    private void setField(Object bean, Field field) {
+        Class<?> fieldType = field.getType();
+        field.setAccessible(true);
+        beans.stream()
+                .filter(fieldType::isInstance)
+                .forEach(ConsumerWrapper.accept(matchBean -> field.set(bean, matchBean)));
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> abstractClass) {
-        return (T) beans.stream()
-                .filter(bean -> matchesBean(abstractClass, bean))
+        return beans.stream()
+                .filter(abstractClass::isInstance)
                 .findFirst()
+                .map(bean -> (T) bean)
                 .orElseThrow(() -> new IllegalArgumentException("no Bean"));
-    }
-
-    private <T> boolean matchesBean(Class<T> abstractClass, Object bean) {
-        Class<?>[] interfaces = bean.getClass().getInterfaces();
-        if (interfaces.length == 0) {
-            return bean.getClass() == abstractClass;
-        }
-        return isInterface(abstractClass, interfaces);
-    }
-
-    private <T> boolean isInterface(Class<T> abstractClass, Class<?>[] interfaces) {
-        for (Class<?> anInterface : interfaces) {
-            if (anInterface == abstractClass) {
-                return true;
-            }
-        }
-        return false;
     }
 }
