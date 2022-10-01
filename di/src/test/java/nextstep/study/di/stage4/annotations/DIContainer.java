e49ee265e7bc0182c17a705952a0710f1bc408b9
BETTERFUTURE4 <rerub0831@gmail.com>
2022-10-01 16:17:32 +0900

    feat: DI 4단계 구현

--- di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
@@ -1,24 +1,57 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import nextstep.study.ConsumerWrapper;
+import nextstep.study.FunctionWrapper;
+import org.reflections.Reflections;
 
-/**
- * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
- */
+
 class DIContainer {
 
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
+    }
+
+    private Set<Object> createBeans(Set<Class<?>> classes) {
+        return classes.stream()
+                .map(FunctionWrapper.apply(Class::getDeclaredConstructor))
+                .peek(constructor -> constructor.setAccessible(true))
+                .map(FunctionWrapper.apply(Constructor::newInstance))
+                .collect(Collectors.toUnmodifiableSet());
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        Set<Class<?>> classes = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        return new DIContainer(classes);
+    }
+
+    private void setFields(final Object bean) {
+        for (final var field : bean.getClass().getDeclaredFields()) {
+            setField(bean, field);
+        }
+    }
+
+    private void setField(Object bean, Field field) {
+        Class<?> fieldType = field.getType();
+        field.setAccessible(true);
+        beans.stream()
+                .filter(fieldType::isInstance)
+                .forEach(ConsumerWrapper.accept(matchBean -> field.set(bean, matchBean)));
     }
 
     @SuppressWarnings("unchecked")
-    public <T> T getBean(final Class<T> aClass) {
-        return null;
+    public <T> T getBean(final Class<T> abstractClass) {
+        return beans.stream()
+                .filter(abstractClass::isInstance)
+                .findFirst()
+                .map(bean -> (T) bean)
+                .orElseThrow(() -> new IllegalArgumentException("no Bean"));
     }
 }
